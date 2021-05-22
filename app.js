e31afe2638b67cb65941e1735d9df357f9009c03
sandyamar <e.sandyareddy@gmail.com>
2021-05-22 11:05:05 +0000

    cannot find module 'express'

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+const dbPath = path.join(__dirname, "cricketTeam.db");
+app.use(express.json());
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+//API 1
+app.get("/players/", async (request, response) => {
+  const getPlayersQuery = `
+    select 
+    * 
+    from 
+    cricket_team 
+    orderBy 
+    player_id;`;
+
+  const playersArray = await db.all(getPlayersQuery);
+  response.send(playersArray);
+});
